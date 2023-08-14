74a7185f77ae04e1e77c7a52212fcbd9fbe22f80
jamesgitonga <gitongajames35@gmail.com>
2023-08-14 08:24:39 +0300

    Delete files (Not working though)

--- app/src/main/java/com/example/audioplayer/MusicAdapter.java
+++ app/src/main/java/com/example/audioplayer/MusicAdapter.java
@@ -1,21 +1,31 @@
 package com.example.audioplayer;
 
+import static java.nio.file.Files.delete;
+
 import android.annotation.SuppressLint;
+import android.content.ContentUris;
 import android.content.Context;
 import android.content.Intent;
 import android.media.MediaMetadataRetriever;
 import android.net.Uri;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.PopupMenu;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.model.Model;
+import com.google.android.material.snackbar.Snackbar;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 
@@ -58,6 +68,36 @@ public class MusicAdapter extends RecyclerView.Adapter<MusicAdapter.MyViewHolder
                 mContext.startActivity(intent);
             }
         });
+        holder.menuMore.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                PopupMenu popupMenu = new PopupMenu(mContext, v);
+                popupMenu.getMenuInflater().inflate(R.menu.popup, popupMenu.getMenu());
+                popupMenu.show();
+                popupMenu.setOnMenuItemClickListener((item -> {
+                    if (item.getItemId() == R.id.delete) {
+                        Toast.makeText(mContext, "Delete Clicked!", Toast.LENGTH_SHORT).show();
+                        deleteFile(position, v);
+                    }
+                    return true;
+                }));
+            }
+        });
+    }
+
+    private void deleteFile(int position, View v) {
+        Uri contentUri = ContentUris.withAppendedId(MediaStore.Audio.Media.EXTERNAL_CONTENT_URI, Long.parseLong(mFiles.get(position).getId()));  // Contains Uri will be used to delete data related to that file in media database
+        File file = new File(mFiles.get(position).getPath());
+        boolean deleted = file.delete();
+        if (deleted) {
+            mContext.getContentResolver().delete(contentUri, null, null);
+            mFiles.remove(position);
+            notifyItemRemoved(position);
+            notifyItemRangeChanged(position, mFiles.size());
+            Snackbar.make(v, "File Deleted : ", Snackbar.LENGTH_LONG).show();
+        } else {
+            Snackbar.make(v, "File Can't Be Deleted : ", Snackbar.LENGTH_LONG).show();  // This else block might be executed if the else block if your file is in sd card
+        }
     }
 
     @Override
@@ -69,12 +109,13 @@ public class MusicAdapter extends RecyclerView.Adapter<MusicAdapter.MyViewHolder
     public class MyViewHolder extends RecyclerView.ViewHolder {
 
         TextView file_name;
-        ImageView album_art;
+        ImageView album_art, menuMore;
 
         public MyViewHolder(@NonNull View itemView) {
             super(itemView);
             file_name = itemView.findViewById(R.id.music_file_name);
             album_art = itemView.findViewById(R.id.music_img);
+            menuMore = itemView.findViewById(R.id.menuMore);
         }
     }
 
